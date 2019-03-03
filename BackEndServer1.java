04102fcda6c508ac3228afbf215fbf3883d6b475
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-03-03 20:49:41 +0000

    Implemented gossip architecture into back end server

--- BackEndServer1.java
+++ BackEndServer1.java
@@ -8,8 +8,15 @@ import java.util.Map;
 	
 public class BackEndServer1 implements BackEndServerInterface {
 
+	/* 
+	============================================
+	Variable definitions
+	============================================
+	*/  
+
 	// Variables relating to the server
 	private String status;
+	private String serverName;
 
 	// Variables relating to Gossip Architecture
 	Map<String, Integer> movieRatings;  // Holds the data
@@ -19,16 +26,33 @@ public class BackEndServer1 implements BackEndServerInterface {
 	ArrayList<Integer> operations = new ArrayList<Integer>();  // Contains a list of updates that have been applied
 	int serverNum;  // Stores the server number
 
-	// Default Constructor
-	public BackEndServer1() {
+
+
+
+
+	/* 
+	============================================
+	Default Constructor
+	============================================
+	*/  
+	public BackEndServer1(String name) {
 		movieRatings = new HashMap<String, Integer>();
 		value_Timestamp = new int[3];
 		replica_Timestamp = new int[3];
 		serverNum = 0;
 		status = "active";
+		serverName = name;
 	}	 
 	
-	// Methods that will be remotely invoked
+
+
+
+
+	/* 
+	============================================
+	Methods that will be remotely invoked
+	============================================
+	*/  
 	public String getStatus(){
 		return status;
 	}
@@ -45,7 +69,7 @@ public class BackEndServer1 implements BackEndServerInterface {
 		if(applyQuery == true) {
 			return movieRatings.get(query.getMovie());
 		}else {
-			// Request gossip messages, then 
+			getGossip();
 		}
 
 		// return value_timestamp to merge, and result of queryy
@@ -79,17 +103,19 @@ public class BackEndServer1 implements BackEndServerInterface {
 			// Applies the update
 			if(applyUpdate = true) {
 				movieRatings.put(update.getMovie(), update.getRating());
-			}
-
-			// Update value_timestamp
-			for(int a = 0; a < value_Timestamp.length; a++) {
-				if(value_Timestamp[a] < ts[a]) {
-					value_Timestamp[a] = ts[a];
+				// Update value_timestamp
+				for(int a = 0; a < value_Timestamp.length; a++) {
+					if(value_Timestamp[a] < ts[a]) {
+						value_Timestamp[a] = ts[a];
+					}
 				}
+				// Added ID to executed table list
+				operations.add(update.getupdateID());
 			}
 
-			// Added ID to executed table list
-			operations.add(update.getupdateID());
+			// If not applied, means there has been an update elsewhere
+			// that has not been recieved by RM
+
 		}
 
 		return ts;
@@ -98,33 +124,142 @@ public class BackEndServer1 implements BackEndServerInterface {
 
 
 
-	// Methods relating to gossip messaging
-	public void sendGossip() {
-		// Sends logRecords and its replica_Timestamp
+
+	/* 
+	============================================
+	Methods relating to gossip messaging
+	============================================
+	*/  
+	
+	// Returns the log record
+	public ArrayList<logRecord> getLogRecord(){
+		return logRecords;
 	}
 
-	public void recieveGossip(ArrayList<logRecord> incoming_logRecords, int[] incoming_replica_Timestamp) {
-		// Merge arriving log with own log
-		// 1. Add record from incoming_logRecords if
+	// Returns the replica timestamp
+	public int[] getReplace_Timestamp(){
+		return replica_Timestamp;
+	}
 
-		
+	// Method gets data from other RM machines to synchronise with
+	public void getGossip(){
+		try{
+			Registry registry = LocateRegistry.getRegistry("127.0.0.1", 8043);
+			String[] serverList = registry.list();
+			for(String registryServerName : serverList){
+				if(!(registryServerName.equals(serverName))){
+					BackEndServerInterface stub = (BackEndServerInterface) registry.lookup(serverName);
+					ArrayList<logRecord> temp_Record = stub.getLogRecord();
+					int[] temp_replica = stub.getReplace_Timestamp();
+					applyGossip(temp_Record, temp_replica);
+				}
+			}
+		} catch (Exception e){
+			System.err.println("Exception in locating server: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+
+	// Method sends current data to other RM machines to synchronise
+	public void sendGossip() {
+		try{
+			Registry registry = LocateRegistry.getRegistry("127.0.0.1", 8043);
+			String[] serverList = registry.list();
+			for(String registryServerName : serverList){
+				if(!(registryServerName.equals(serverName))){
+					BackEndServerInterface stub = (BackEndServerInterface) registry.lookup(serverName);
+					stub.applyGossip(logRecords, replica_Timestamp);
+				}
+			}
+		} catch (Exception e){
+			System.err.println("Exception in locating server: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+
+	// Method takes data from another RM and
+	public void applyGossip(ArrayList<logRecord> incoming_logRecords, int[] incoming_replica_Timestamp) {
+		// Merge arriving log with own log
+		// 1. Add record from incoming_logRecords if record timestamp is less than current replica_Timestamp
+		for(logRecord log : incoming_logRecords) {
+			boolean addLog = false;
+			for(int a = 0; a < replica_Timestamp.length; a++) {
+				if(log.getTS()[a] > replica_Timestamp[a]){
+					addLog = true;
+				}
+			}
+			if(addLog = true){
+				logRecords.add(log);
+			}
+		}
+
+		// 2. RM merges replica_timestamp with incoming timestamp
+		for(int a = 0; a < incoming_replica_Timestamp.length; a++) {
+			if(replica_Timestamp[a] < incoming_replica_Timestamp[a]){
+				replica_Timestamp[a] = incoming_replica_Timestamp[a];
+			}
+		}
 
 		// Apply updates that have become stable and not yet executed
+		ArrayList<logRecord> stableUpdates = new ArrayList<logRecord>();
+
+		// 3: Loop through RM log of updates and add stable ones
+		for(logRecord log : logRecords){
+			// If update has not already been applied
+			if(!(operations.contains(log.getUpdate().getupdateID()))) {
+				// If the update is stable
+				boolean stable = true;
+				for(int a = 0; a < value_Timestamp.length; a++){
+					if(log.getUpdate().getPrev()[a] > value_Timestamp[a]) {
+						stable = false;
+					}
+				}
+				if(stable == true){
+					stableUpdates.add(log);
+				}
+			}
+		}		
+		
+		// 4: Order the logs into suitable execution order (NOT IMPLEMENTED)
+		/*for(int a = 0; a < stableUpdates.size(); a++){
+			for(int b = 0; b < stableUpdates.size() - a; b++) {
+				for(int c = 0; c < )
+			}
+		}*/
+
+		// 5: Apply the stable updates
+		for(logRecord log : stableUpdates){
+			movieRatings.put(log.getUpdate().getMovie(), log.getUpdate().getRating());
+			// Update value_timestamp
+			for(int a = 0; a < value_Timestamp.length; a++) {
+				if(value_Timestamp[a] < log.getTS()[a]) {
+					value_Timestamp[a] = log.getTS()[a];
+				}
+			}
+			// Added ID to executed table list
+			operations.add(log.getUpdate().getupdateID());
+		}
 
 		// Eliminate records from log and executed table 
-		// when they have been known to be applied everywhere
+		// when they have been known to be applied everywhere (NOT IMPLEMENTED)
 	}
 
 
-	// Main method: 
-	// Instatntiates and registers an instance of the server with the rmi registry
+
+
+
+	/* 
+	============================================
+	Method that creates an instance of the front end object
+	============================================
+	*/  
     public static void main(String args[]) { 
 			try {
 				// Defines the server name
 				String name = "backEnd1";
 				
 				// Create server object
-				BackEndServer1 obj = new BackEndServer1();
+				BackEndServer1 obj = new BackEndServer1(name);
 
 				// Create remote object stub from server object
 				BackEndServerInterface stub = (BackEndServerInterface) UnicastRemoteObject.exportObject(obj, 0);
--- BackEndServerInterface.java
+++ BackEndServerInterface.java
@@ -1,8 +1,13 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface BackEndServerInterface extends Remote {    
     public String getStatus() throws RemoteException;
     public int retrieveRating(queryRequest query) throws RemoteException;
     public int[] updateRating(updateRequest update) throws RemoteException;
+    public void applyGossip(ArrayList<logRecord> incoming_logRecords, int[] incoming_replica_Timestamp) throws RemoteException;
+
+    public ArrayList<logRecord> getLogRecord() throws RemoteException;
+    public int[] getReplace_Timestamp() throws RemoteException;
 }
